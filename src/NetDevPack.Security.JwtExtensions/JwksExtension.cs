51ea2cc34cb3ba540dce7aec1c7ce6463273d6e1
Bruno Brito <bhdebrito@gmail.com>
2020-08-05 02:46:20 -0300

    refact classes

--- src/NetDevPack.Security.JwtExtensions/JwksExtension.cs
+++ src/NetDevPack.Security.JwtExtensions/JwksExtension.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace NetDevPack.Security.JwtExtensions
 {
@@ -11,47 +11,18 @@ namespace NetDevPack.Security.JwtExtensions
     {
         public static void SetJwksOptions(this JwtBearerOptions options, JwkOptions jwkOptions)
         {
+            var httpClient = new HttpClient(options.BackchannelHttpHandler ?? new HttpClientHandler())
+            {
+                Timeout = options.BackchannelTimeout,
+                MaxResponseContentBufferSize = 1024 * 1024 * 10 // 10 MB 
+            };
 
-            if (options.TokenValidationParameters == null)
-                options.TokenValidationParameters = new TokenValidationParameters();
-
-            if (options.TokenValidationParameters.IssuerSigningKeyResolver == null)
-                options.TokenValidationParameters.IssuerSigningKeyResolver = new JwkRetriever(jwkOptions).IssuerSigningKeyResolver;
-
+            options.ConfigurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+                jwkOptions.JwksUri.OriginalString,
+                new JwksRetriever(),
+                new HttpDocumentRetriever(httpClient) { RequireHttps = options.RequireHttpsMetadata });
             options.TokenValidationParameters.ValidateAudience = false;
             options.TokenValidationParameters.ValidIssuer = jwkOptions.Issuer;
-        }
-
-        public class JwkRetriever
-        {
-            private static readonly HttpClient HttpClient = new HttpClient();
-
-            public JwkRetriever(JwkOptions jwkOptions)
-            {
-                Options = jwkOptions;
-            }
-
-            public JwkOptions Options { get; }
-            public JwkList LastResponse { get; private set; }
-            public IEnumerable<SecurityKey> IssuerSigningKeyResolver(string token, SecurityToken securityToken, string kid, TokenValidationParameters validationParameters)
-            {
-                if (LastResponse == null || LastResponse.When.Add(Options.KeepFor) < DateTime.Now)
-                {
-                    var jwkTask = GetJwks();
-                    jwkTask.Wait();
-                    LastResponse = new JwkList(jwkTask.Result);
-                }
-
-                return LastResponse.Jwks.Keys;
-            }
-
-            private async Task<JsonWebKeySet> GetJwks()
-            {
-                var response = await HttpClient.GetAsync(Options.JwksUri);
-                var responseString = await response.Content.ReadAsStringAsync();
-                return new JsonWebKeySet(responseString);
-
-            }
         }
     }
 }
