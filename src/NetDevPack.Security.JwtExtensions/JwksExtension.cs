d940668a7a1eec7eb492ed534800d1419fd3f2d1
Bruno Brito <bhdebrito@gmail.com>
2020-08-05 02:37:28 -0300

    ASP.NET Member new directions

--- src/NetDevPack.Security.JwtExtensions/JwksExtension.cs
+++ src/NetDevPack.Security.JwtExtensions/JwksExtension.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
 namespace NetDevPack.Security.JwtExtensions
@@ -7,8 +11,6 @@ namespace NetDevPack.Security.JwtExtensions
     {
         public static void SetJwksOptions(this JwtBearerOptions options, JwkOptions jwkOptions)
         {
-            options.Authority = null;
-            options.Audience = null;
 
             if (options.TokenValidationParameters == null)
                 options.TokenValidationParameters = new TokenValidationParameters();
@@ -18,6 +20,38 @@ namespace NetDevPack.Security.JwtExtensions
 
             options.TokenValidationParameters.ValidateAudience = false;
             options.TokenValidationParameters.ValidIssuer = jwkOptions.Issuer;
+        }
+
+        public class JwkRetriever
+        {
+            private static readonly HttpClient HttpClient = new HttpClient();
+
+            public JwkRetriever(JwkOptions jwkOptions)
+            {
+                Options = jwkOptions;
+            }
+
+            public JwkOptions Options { get; }
+            public JwkList LastResponse { get; private set; }
+            public IEnumerable<SecurityKey> IssuerSigningKeyResolver(string token, SecurityToken securityToken, string kid, TokenValidationParameters validationParameters)
+            {
+                if (LastResponse == null || LastResponse.When.Add(Options.KeepFor) < DateTime.Now)
+                {
+                    var jwkTask = GetJwks();
+                    jwkTask.Wait();
+                    LastResponse = new JwkList(jwkTask.Result);
+                }
+
+                return LastResponse.Jwks.Keys;
+            }
+
+            private async Task<JsonWebKeySet> GetJwks()
+            {
+                var response = await HttpClient.GetAsync(Options.JwksUri);
+                var responseString = await response.Content.ReadAsStringAsync();
+                return new JsonWebKeySet(responseString);
+
+            }
         }
     }
 }
---
+++ src/NetDevPack.Security.JwtExtensions/JwksRetriever.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+namespace NetDevPack.Security.JwtExtensions
+{
+    public class JwksRetriever : IConfigurationRetriever<OpenIdConnectConfiguration>
+    {
+        public Task<OpenIdConnectConfiguration> GetConfigurationAsync(string address, IDocumentRetriever retriever, CancellationToken cancel)
+        {
+            return GetAsync(address, retriever, cancel);
+        }
+
+        /// <summary>
+        /// Retrieves a populated <see cref="OpenIdConnectConfiguration"/> given an address and an <see cref="IDocumentRetriever"/>.
+        /// </summary>
+        /// <param name="address">address of the jwks uri.</param>
+        /// <param name="retriever">the <see cref="IDocumentRetriever"/> to use to read the jwks</param>
+        /// <param name="cancel"><see cref="CancellationToken"/>.</param>
+        /// <returns>A populated <see cref="OpenIdConnectConfiguration"/> instance.</returns>
+        public static async Task<OpenIdConnectConfiguration> GetAsync(string address, IDocumentRetriever retriever, CancellationToken cancel)
+        {
+            if (string.IsNullOrWhiteSpace(address))
+                throw LogHelper.LogArgumentNullException(nameof(address));
+
+            if (retriever == null)
+                throw LogHelper.LogArgumentNullException(nameof(retriever));
+
+            var doc = await retriever.GetDocumentAsync(address, cancel).ConfigureAwait(false);
+            LogHelper.LogVerbose("IDX21811: Deserializing the string: '{0}' obtained from metadata endpoint into openIdConnectConfiguration object.", doc);
+            var jwks = new JsonWebKeySet(doc);
+            var openIdConnectConfiguration = new OpenIdConnectConfiguration()
+            {
+                JsonWebKeySet = jwks,
+                JwksUri = address,
+            };
+            foreach (var securityKey in jwks.GetSigningKeys())
+                openIdConnectConfiguration.SigningKeys.Add(securityKey);
+
+            return openIdConnectConfiguration;
+        }
+    }
+}
